490f6d9048ecd95e0a0cf3e46cb162441f887fe6
Cheng Fuyu <c771454280@gmail.com>
2026-06-22 06:21:16 +0800

    Update 2356.sql

--- 01-sql/leetcode-sql/2356.sql
+++ 01-sql/leetcode-sql/2356.sql
@@ -1,1 +1,16 @@
+-- LeetCode 2356. 每位教师所教授的科目种类的数量
+-- Source: LeetCode
+-- Difficulty: Easy
+-- Topic: GROUP BY, COUNT DISTINCT
+-- Database: MySQL
 
+SELECT 
+    teacher_id,
+    COUNT(DISTINCT subject_id) AS cnt
+FROM teacher
+GROUP BY teacher_id;
+
+-- 查询每位老师在大学里教授的科目种类数量。
+-- 确定按照每位老师进行分组 即 GROUP BY teacher_id
+-- 分组条件: 所教授的科目种类(1)的数量(2) 即 subject_id 
+  -- 并过滤重复的科目种类+count个数 即 COUNT(DISTINCT subject_id)
